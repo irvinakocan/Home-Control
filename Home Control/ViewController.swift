851987c5e7b8e2a80433e73420cfef2a45fbd206
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-06-13 21:07:25 +0200

    Initial Commit

---
+++ Home Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Control
+//
+//  Created by Macbook Air 2017 on 13. 6. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
